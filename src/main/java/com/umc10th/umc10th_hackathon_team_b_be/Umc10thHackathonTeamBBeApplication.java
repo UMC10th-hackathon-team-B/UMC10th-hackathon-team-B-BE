aa3506555c3018108ccaa0915dbebc49742eaa57
SUNG HYUNJOON <tjdguswns1941@gachon.ac.kr>
2026-06-16 15:08:00 +0900

    first commit

---
+++ src/main/java/com/umc10th/umc10th_hackathon_team_b_be/Umc10thHackathonTeamBBeApplication.java
@@ -0,0 +1,13 @@
+package com.umc10th.umc10th_hackathon_team_b_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc10thHackathonTeamBBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc10thHackathonTeamBBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umc10th/umc10th_hackathon_team_b_be/Umc10thHackathonTeamBBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc10th.umc10th_hackathon_team_b_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc10thHackathonTeamBBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
